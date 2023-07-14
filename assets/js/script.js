f8d9257d8781ad8deefb0983265e628c72c55569
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-13 23:37:58 -0500

    Early JS. More CSS and HTML styling.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 function weatherBalloon( cityID ) {
-  var key = '293f455dd8c54c0b165da1f617f1f056';
+  var key = '{293f455dd8c54c0b165da1f617f1f056}';
   fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
   .then(function(resp) { return resp.json() }) // Convert data to json
   .then(function(data) {

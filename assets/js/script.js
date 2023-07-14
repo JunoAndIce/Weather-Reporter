c9e7af7a2be3c82fcb3e2727c632fc20e8e8919a
Juno & Ice <70862998+JunoAndIce@users.noreply.github.com>
2023-07-14 10:18:39 -0500

    Todolist added. Early JS API call added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 function weatherBalloon( cityID ) {
-  var key = '{293f455dd8c54c0b165da1f617f1f056}';
+  var key = '293f455dd8c54c0b165da1f617f1f056';
   fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID+ '&appid=' + key)  
   .then(function(resp) { return resp.json() }) // Convert data to json
   .then(function(data) {
@@ -10,6 +10,15 @@ function weatherBalloon( cityID ) {
   });
 }
 
-window.onload = function() {
-  weatherBalloon( 6167865 );
-}
+// window.onload = function() {
+//   weatherBalloon( 6167865 );
+// }
+
+// $()
+
+// TODO: GET VALUE FROM SEARCH BAR
+// TODO: PASS VALUE TO WEATHER
+// TODO: CHANGE HTML VALUE/DATA WITH DATA PULLED FROM API
+// TODO: REMEMBER LAST SEARCH AND CREATE A BUTTON IN PAST SEARCHES
+// TODO: GIVE BUTTON VALUE TO CHANGE DATA TO THAT CITY
+// TODO: **EXTRA** MOVE BUTTON TO THE TOP OF LIST
